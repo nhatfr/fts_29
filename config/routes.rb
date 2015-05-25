f9a7f3608fd0c0ee2ad3a3b2162b8be0bcf770f5
Nhat Tran Van <tran.van.nhat@framgia.com>
2015-05-25 14:22:13 +0700

    use devise to finish log-in/logout and sign-up

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {sessions: "devise/sessions"}
   root 'static_pages#index'
+
+   devise_scope :user do
+    get "/logout"    => "devise/sessions#destroy"
+   end
 end
