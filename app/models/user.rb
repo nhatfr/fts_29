2268910e84a3f73819acabfe4bfab14861906e5b
Nhat Tran Van <tran.van.nhat@framgia.com>
2015-05-25 18:36:52 +0700

    Fix some misstake

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :registerable,
+  devise :registerable, :database_authenticatable,
          :recoverable, :rememberable, :validatable, :confirmable
   has_many :exams, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {sessions: "devise/sessions"}
+  devise_for :users
   root 'static_pages#index'
-
-   devise_scope :user do
-    get "/logout"    => "devise/sessions#destroy"
-   end
+  devise_scope :user do
+    get "login" => "devise/sessions#new"
+    get "logout" => "devise/sessions#destroy"
+    get "sign_up" => "devise/registration#new"
+  end
 end
